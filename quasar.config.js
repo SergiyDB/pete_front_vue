0567bb3fe1734ea3cb65e26b4b69b78c0c55baa8
SergiyDB <sergiy.dmitrievich@fractalsoft.ec>
2024-07-15 16:32:03 -0500

    Dist folder was changed

--- quasar.config.js
+++ quasar.config.js
@@ -50,7 +50,7 @@ module.exports = configure(function (/* ctx */) {
         browser: [ 'es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1' ],
         node: 'node20',
         publicPath: '/',
-        distDir: 'dist/spa',
+        distDir: 'dist',
         vueRouterMode: 'history', // если используете режим истории роутера
       },
 
